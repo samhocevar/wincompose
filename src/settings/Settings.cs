0541c9db1b9919264445eeb2e23cf60cb00485e0
Sam Hocevar <sam@hocevar.net>
2015-03-30 22:12:21 +0200

    Make InsertZwsp default to false until the feature is robust and non-intrusive. Closes #75.

--- src/settings/Settings.cs
+++ src/settings/Settings.cs
@@ -39,7 +39,7 @@ namespace WinCompose
             DiscardOnInvalid = new SettingsEntry<bool>(GlobalSection, "discard_on_invalid", false);
             BeepOnInvalid = new SettingsEntry<bool>(GlobalSection, "beep_on_invalid", false);
             KeepOriginalKey = new SettingsEntry<bool>(GlobalSection, "keep_original_key", false);
-            InsertZwsp = new SettingsEntry<bool>(GlobalSection, "insert_zwsp", true);
+            InsertZwsp = new SettingsEntry<bool>(GlobalSection, "insert_zwsp", false);
             EmulateCapsLock = new SettingsEntry<bool>(GlobalSection, "emulate_capslock", false);
             ShiftDisablesCapsLock = new SettingsEntry<bool>(GlobalSection, "shift_disables_capslock", false);
         }
