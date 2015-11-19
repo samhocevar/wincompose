f0c41cb10d3d94d08f7bbc478a6089ca02f1a037
Sam Hocevar <sam@hocevar.net>
2015-11-19 19:56:24 +0100

    Cosmetic: reduce indentation in Updater.cs

--- src/composer/Updater.cs
+++ src/composer/Updater.cs
@@ -20,88 +20,90 @@ using System.Threading;
 
 namespace WinCompose
 {
-    static class Updater
+
+static class Updater
+{
+    public static void Init()
     {
-        public static void Init()
-        {
-            m_thread = new Thread(() => { Updater.Run(); });
-            m_thread.Start();
-        }
+        m_thread = new Thread(() => { Updater.Run(); });
+        m_thread.Start();
+    }
 
-        public static void Fini()
-        {
-            m_thread.Interrupt();
-            m_thread.Join();
-        }
+    public static void Fini()
+    {
+        m_thread.Interrupt();
+        m_thread.Join();
+    }
 
-        private static void Run()
-        {
-            for (;;)
-            {
-                try
-                {
-                    GetStatusData();
-
-                    if (HasNewerVersion())
-                    {
-                    }
-
-                    // Sleep between 30 and 90 minutes before querying again
-                    Thread.Sleep(new Random().Next(30, 90) * 60 * 1000);
-                }
-                catch (ThreadInterruptedException)
-                {
-                    return;
-                }
-            }
-        }
-
-        private static bool HasNewerVersion()
-        {
-            if (m_data.ContainsKey("Latest"))
-            {
-                ;
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Query the WinCompose website for update information
-        /// </summary>
-        private static void GetStatusData()
+    private static void Run()
+    {
+        for (;;)
         {
             try
             {
-                WebClient browser = new WebClient();
-                browser.Headers.Add("user-agent", GetUserAgent());
-                Stream s = browser.OpenRead("http://wincompose.info/status.txt");
-                StreamReader sr = new StreamReader(s);
+                GetStatusData();
 
-                for (string line = sr.ReadLine(); line != null;  line = sr.ReadLine())
+                if (HasNewerVersion())
                 {
-                    string pattern = "^([^#: ][^: ]*):  *(.*[^ ]) *$";
-                    var m = Regex.Match(line, pattern);
-                    if (m.Groups.Count == 3)
-                        m_data[m.Groups[1].Captures[0].ToString()] = m.Groups[2].Captures[0].ToString();
                 }
 
-                sr.Close();
-                s.Close();
+                // Sleep between 30 and 90 minutes before querying again
+                Thread.Sleep(new Random().Next(30, 90) * 60 * 1000);
             }
-            catch(Exception) {}
+            catch (ThreadInterruptedException)
+            {
+                return;
+            }
         }
-
-        private static string GetUserAgent()
-        {
-            return string.Format("WinCompose/{0} ({1}{2})",
-                                 Settings.Version,
-                                 Environment.OSVersion,
-                                 Settings.IsDebugging() ? "; Development" :
-                                 Settings.IsInstalled() ? "" : "; Portable");
-        }
-
-        private static Dictionary<string, string> m_data = new Dictionary<string, string>();
-        private static Thread m_thread;
     }
+
+    private static bool HasNewerVersion()
+    {
+        if (m_data.ContainsKey("Latest"))
+        {
+            ;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Query the WinCompose website for update information
+    /// </summary>
+    private static void GetStatusData()
+    {
+        try
+        {
+            WebClient browser = new WebClient();
+            browser.Headers.Add("user-agent", GetUserAgent());
+            Stream s = browser.OpenRead("http://wincompose.info/status.txt");
+            StreamReader sr = new StreamReader(s);
+
+            for (string line = sr.ReadLine(); line != null;  line = sr.ReadLine())
+            {
+                string pattern = "^([^#: ][^: ]*):  *(.*[^ ]) *$";
+                var m = Regex.Match(line, pattern);
+                if (m.Groups.Count == 3)
+                    m_data[m.Groups[1].Captures[0].ToString()] = m.Groups[2].Captures[0].ToString();
+            }
+
+            sr.Close();
+            s.Close();
+        }
+        catch(Exception) {}
+    }
+
+    private static string GetUserAgent()
+    {
+        return string.Format("WinCompose/{0} ({1}{2})",
+                             Settings.Version,
+                             Environment.OSVersion,
+                             Settings.IsDebugging() ? "; Development" :
+                             Settings.IsInstalled() ? "" : "; Portable");
+    }
+
+    private static Dictionary<string, string> m_data = new Dictionary<string, string>();
+    private static Thread m_thread;
+}
+
 }
 
