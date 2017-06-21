595ba131bf7c90ff672470f6a3c8caa1075b7573
Sam Hocevar <sam@hocevar.net>
2017-06-21 23:02:46 +0200

    Disable the “always on systray” feature for now.
    This has proven a bit controversial in the beta releases; I will
    definitely not make it the default behaviour.

--- src/composer/SysTrayIcon.xaml.cs
+++ src/composer/SysTrayIcon.xaml.cs
@@ -58,7 +58,8 @@ namespace WinCompose
             m_icon.Click += NotifyiconClicked;
             m_icon.DoubleClick += NotifyiconDoubleclicked;
 
-            SysTray.AlwaysShow("wincompose[.]exe");
+            // XXX: disabled for now, as this feature is a bit controversial
+            //SysTray.AlwaysShow("wincompose[.]exe");
 
             Composer.Changed += SysTrayUpdateCallback;
             Updater.Changed += SysTrayUpdateCallback;
