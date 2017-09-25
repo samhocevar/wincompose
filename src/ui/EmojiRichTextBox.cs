7a1f0c022f71f86505fc7159d987f501877258f3
Sam Hocevar <sam@hocevar.net>
2017-09-25 12:23:18 +0200

    Move ColorTypeface to its own source file.

--- src/ui/EmojiRichTextBox.cs
+++ src/ui/EmojiRichTextBox.cs
@@ -11,8 +11,6 @@
 //
 
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -22,26 +20,9 @@ using System.Windows.Threading;
 
 namespace WinCompose
 {
-    public class BEBinaryReader : BinaryReader
-    {
-        public BEBinaryReader(Stream s) : base(s) {}
-
-        public override short  ReadInt16()  => BitConverter.ToInt16(ReadReverse(2), 0);
-        public override ushort ReadUInt16() => BitConverter.ToUInt16(ReadReverse(2), 0);
-        public override int    ReadInt32()  => BitConverter.ToInt32(ReadReverse(4), 0);
-        public override uint   ReadUInt32() => BitConverter.ToUInt32(ReadReverse(4), 0);
-        public override long   ReadInt64()  => BitConverter.ToInt64(ReadReverse(8), 0);
-        public override ulong  ReadUInt64() => BitConverter.ToUInt64(ReadReverse(8), 0);
-
-        private byte[] ReadReverse(int count)
-        {
-            var b = ReadBytes(count); Array.Reverse(b); return b;
-        }
-    }
-
     public class ColorGlyph : Canvas
     {
-        public ColorGlyph(ColorFont font, int codepoint)
+        public ColorGlyph(ColorTypeface font, int codepoint)
         {
             m_font = font;
             m_codepoint = codepoint;
@@ -49,154 +30,17 @@ namespace WinCompose
 
         protected override void OnRender(DrawingContext dc)
         {
-            m_font.RenderGlyph(dc, m_codepoint, Width);
+            m_font.RenderGlyph(dc, m_codepoint, Width, new Point(-5, 10));
         }
 
-        private ColorFont m_font;
+        private ColorTypeface m_font;
         private int m_codepoint;
-    }
-
-    public class ColorFont
-    {
-        public ColorFont(string name)
-        {
-            Typeface tf = new Typeface(name);
-            if (tf.TryGetGlyphTypeface(out m_gtf))
-            {
-                using (var s = m_gtf.GetFontStream())
-                {
-                    ReadFontStream(s);
-                }
-            }
-        }
-
-        public void RenderGlyph(DrawingContext dc, int codepoint, double size)
-        {
-            ushort gid = m_gtf.CharacterToGlyphMap[codepoint];
-            int start = m_layer_indices[gid], stop = start + m_layer_counts[gid];
-
-            Random rand = new Random();
-
-            for (int i = start; i < stop; ++i)
-            {
-                GlyphRun r = new GlyphRun(m_gtf, 0, false, size, // pt size
-                                          new ushort[] { m_glyph_layers[i] },
-                                          new Point(-5, 22),
-                                          new double[] { 0 }, // advance
-                                          null, null, null, // FIXME: check what this is?
-                                          null, null, null);
-                int palette = 0; // FIXME: support multiple palettes?
-                Brush b = new SolidColorBrush(m_colors[m_palettes[palette] + m_glyph_palettes[i]]);
-
-                dc.DrawGlyphRun(b, r);
-            }
-        }
-
-        public bool HasCodepoint(int codepoint)
-        {
-            // Check that the character is mapped to a glyph, and that the glyph
-            // has colour information. Otherwise, we are not interested.
-            ushort glyph;
-            return m_gtf.CharacterToGlyphMap.TryGetValue(codepoint, out glyph)
-                    && m_layer_indices.ContainsKey(glyph);
-        }
-
-        private void ReadFontStream(Stream s)
-        {
-            int colr_offset = -1, colr_length = -1;
-            int cpal_offset = -1, cpal_length = -1;
-
-            // Read font header
-            var b = new BEBinaryReader(s);
-            uint header = b.ReadUInt32();
-            int table_count = b.ReadUInt16();
-            b.BaseStream.Seek(6, SeekOrigin.Current);
-
-            // Read available table information
-            for (int i = 0; i < table_count; ++i)
-            {
-                int table_header = b.ReadInt32();
-                int table_checksum = b.ReadInt32();
-                int table_offset = b.ReadInt32();
-                int table_length = b.ReadInt32();
-
-                if (table_header == 0x434f4c52) // COLR
-                {
-                    colr_offset = table_offset;
-                    colr_length = table_length;
-                }
-                else if (table_header == 0x4350414c) // CPAL
-                {
-                    cpal_offset = table_offset;
-                    cpal_length = table_length;
-                }
-            }
-
-            if (colr_offset != -1 && cpal_offset != -1)
-            {
-                // Read the COLR table
-                b.BaseStream.Seek(colr_offset, SeekOrigin.Begin);
-                ushort version = b.ReadUInt16();
-                int glyph_count = b.ReadUInt16();
-                int glyphs_offset = b.ReadInt32();
-                int layers_offset = b.ReadInt32();
-                int layer_count = b.ReadUInt16();
-
-                b.BaseStream.Seek(colr_offset + glyphs_offset, SeekOrigin.Begin);
-                for (int i = 0; i < glyph_count; ++i)
-                {
-                    ushort gid = b.ReadUInt16();
-                    m_layer_indices[gid] = b.ReadUInt16();
-                    m_layer_counts[gid] = b.ReadUInt16();
-                }
-
-                b.BaseStream.Seek(colr_offset + layers_offset, SeekOrigin.Begin);
-                m_glyph_layers = new ushort[layer_count];
-                m_glyph_palettes = new ushort[layer_count];
-                for (int i = 0; i < layer_count; ++i)
-                {
-                    m_glyph_layers[i] = b.ReadUInt16();
-                    m_glyph_palettes[i] = b.ReadUInt16();
-                }
-
-                // Read the CPAL table
-                b.BaseStream.Seek(cpal_offset, SeekOrigin.Begin);
-                ushort palette_version = b.ReadUInt16();
-                int entry_count = b.ReadUInt16();
-                int palette_count = b.ReadUInt16();
-                int color_count = b.ReadUInt16();
-                int colors_offset = b.ReadInt32();
-
-                m_palettes = new ushort[palette_count];
-                for (int i = 0; i < palette_count; ++i)
-                {
-                    m_palettes[i] = b.ReadUInt16();
-                }
-
-                b.BaseStream.Seek(cpal_offset + colors_offset, SeekOrigin.Begin);
-                m_colors = new Color[color_count];
-                for (int i = 0; i < color_count; ++i)
-                {
-                    byte[] tmp = b.ReadBytes(4);
-                    m_colors[i] = Color.FromArgb(tmp[3], tmp[2], tmp[1], tmp[0]);
-                }
-            }
-        }
-
-        private GlyphTypeface m_gtf;
-
-        private Dictionary<ushort, ushort> m_layer_indices = new Dictionary<ushort, ushort>();
-        private Dictionary<ushort, ushort> m_layer_counts = new Dictionary<ushort, ushort>();
-        private ushort[] m_glyph_layers = new ushort[0];
-        private ushort[] m_glyph_palettes = new ushort[0];
-        private ushort[] m_palettes = new ushort[0];
-        private Color[] m_colors = new Color[0];
     }
 
     // Inheriting from Span makes it easy to parse the tree for copy-paste
     public class Emoji : Span
     {
-        static ColorFont m_font = new ColorFont("Segoe UI Emoji");
+        static ColorTypeface m_font = new ColorTypeface("Segoe UI Emoji");
 
         // Need an empty constructor for serialisation (undo/redo)
         public Emoji() {}
