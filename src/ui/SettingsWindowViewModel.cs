1f0ba2e912b0041a8923e45cf9ca414678c9b1c0
Sam Hocevar <sam@hocevar.net>
2017-11-08 20:24:48 +0100

    Fix a very old bug with the compose key not being saved.
    This happened when I added support for multiple compose keys (still not
    supported in the GUI, but the config file has it).

--- src/ui/SettingsWindowViewModel.cs
+++ src/ui/SettingsWindowViewModel.cs
@@ -12,62 +12,65 @@
 //
 
 using System.Windows;
-using System.ComponentModel;
-using System.Windows.Input;
 using WinCompose.i18n;
 
 namespace WinCompose
 {
-    public partial class SettingsWindowViewModel : ViewModelBase
+    public class SettingsWindowViewModel : ViewModelBase
     {
-        private DelegateCommand    m_close_command;
-        private string             m_selected_language;
-        private string             m_close_button_text;
-        private Visibility         m_warn_message_visibility;
+        private DelegateCommand m_close_command;
+        private string m_selected_language;
+        private string m_close_button_text;
+        private Visibility m_warn_message_visibility;
 
         public SettingsWindowViewModel()
         {
-            m_close_command             = new DelegateCommand(OnCloseCommandExecuted);
-            m_selected_language         = Settings.Language.Value;
-            m_close_button_text         = Text.Close;
-            m_warn_message_visibility   = Visibility.Collapsed;
+            m_close_command = new DelegateCommand(OnCloseCommandExecuted);
+            m_selected_language = Settings.Language.Value;
+            m_close_button_text = Text.Close;
+            m_warn_message_visibility = Visibility.Collapsed;
         }
 
         public DelegateCommand CloseButtonCommand
         {
-            get { return m_close_command; }
-            private set { SetValue(ref m_close_command, value, "CloseButtonCommand"); }
+            get => m_close_command;
+            private set => SetValue(ref m_close_command, value, "CloseButtonCommand");
         }
 
         public string SelectedLanguage
         {
-            get { return m_selected_language; }
-            set { SetValue(ref m_selected_language, value, "SelectedLanguage"); }
+            get => m_selected_language;
+            set => SetValue(ref m_selected_language, value, "SelectedLanguage");
         }
 
         public Key ComposeKey0
         {
-            get { return Settings.ComposeKeys.Value[0]; }
-            set { Settings.ComposeKeys.Value[0] = value; }
+            get => Settings.ComposeKeys.Value[0];
+            set
+            {
+                // Make sure the value is saved when the user changes it
+                var keys = new KeySequence(Settings.ComposeKeys.Value) { [0] = value };
+                Settings.ComposeKeys.Value = keys;
+            }
         }
 
         public string CloseButtonText
         {
-            get { return m_close_button_text; }
-            private set { SetValue(ref m_close_button_text, value, "CloseButtonText"); }
+            get => m_close_button_text;
+            private set => SetValue(ref m_close_button_text, value, "CloseButtonText");
         }
 
         public Visibility WarnMessageVisibility
         {
-            get { return m_warn_message_visibility; }
-            set { SetValue(ref m_warn_message_visibility, value, "WarnMessageVisibility"); }
+            get => m_warn_message_visibility;
+            set => SetValue(ref m_warn_message_visibility, value, "WarnMessageVisibility");
         }
 
         protected override void OnPropertyChanged(string propertyName)
         {
             base.OnPropertyChanged(propertyName);
 
-            if(propertyName == "SelectedLanguage")
+            if (propertyName == "SelectedLanguage")
             {
                 Settings.Language.Value = SelectedLanguage;
                 WarnMessageVisibility   = Visibility.Visible;
@@ -76,12 +79,12 @@ namespace WinCompose
             }
         }
 
-        private void OnCloseCommandExecuted(object parameter)
+        private static void OnCloseCommandExecuted(object parameter)
         {
             ((Window)parameter).Hide();
         }
 
-        private void OnRestartCommandExecuted(object parameter)
+        private static void OnRestartCommandExecuted(object parameter)
         {
             System.Windows.Forms.Application.Restart();
         }
