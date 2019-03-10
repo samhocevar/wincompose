b5b4284c44efae12c90e85e3ec0ca253c6d4de4f
Sam Hocevar <sam@hocevar.net>
2019-03-10 22:49:14 +0100

    Move interop classes and types to a separate directory.
    The “composer” directory was starting to become a bit of a mess.

--- src/interop/RemoteControl.cs
+++ src/interop/RemoteControl.cs
@@ -2,7 +2,6 @@
 //  WinCompose — a compose key for Windows — http://wincompose.info/
 //
 //  Copyright © 2013—2019 Sam Hocevar <sam@hocevar.net>
-//              2014—2015 Benjamin Litzelmann
 //
 //  This program is free software. It comes without any warranty, to
 //  the extent permitted by applicable law. You can redistribute it
--- src/interop/RemoteTypes.cs
+++ src/interop/RemoteTypes.cs
@@ -1,8 +1,7 @@
 ﻿//
 //  WinCompose — a compose key for Windows — http://wincompose.info/
 //
-//  Copyright © 2013—2018 Sam Hocevar <sam@hocevar.net>
-//              2014—2015 Benjamin Litzelmann
+//  Copyright © 2013—2019 Sam Hocevar <sam@hocevar.net>
 //
 //  This program is free software. It comes without any warranty, to
 //  the extent permitted by applicable law. You can redistribute it
