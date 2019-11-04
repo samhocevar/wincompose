samhocevar/wincompose

commit 4b6e5d607e48c5a319a31be5e214febe8111f297

Fix AltGr handling in various situations.
I believe I found a more robust way to detect that a Left Control event is
actually part of AltGr and thus should be ignored.

This addresses #279, probably addresses #274, #281, #236, and possibly
addresses #192.

src/composer/Composer.cs
@@ -131,6 +131,30 @@ static class Composer
         // to its virtual key code.
         Key key = KeyboardLayout.VkToKey(vk, sc, flags, has_shift, has_altgr, has_capslock);
 
+        // Special handling of Left Control on keyboards with AltGr
+        if (KeyboardLayout.HasAltGr && key.VirtualKey == VK.LCONTROL)
+        {
+            // If this is a key down event with LLKHF_ALTDOWN but no Alt key
+            // is down, it is actually AltGr.
+            if (is_keydown && (flags & LLKHF.ALTDOWN) != 0
+                 && ((NativeMethods.GetKeyState(VK.LMENU) |
+                      NativeMethods.GetKeyState(VK.RMENU)) & 0x80) == 0)
+            {
+                m_control_key_was_altgr = true;
+                return true;
+            }
+
+            // If this is a key up event bug Left Control is not down, it is
+            // actually AltGr.
+            if (is_keyup && m_control_key_was_altgr)
+            {
+                m_control_key_was_altgr = false;
+                return true;
+            }
+
+            m_control_key_was_altgr = false;
+        }
+
         // If Caps Lock is on, and the Caps Lock hack is enabled, we check
         // whether this key without Caps Lock gives a non-ASCII alphabetical
         // character. If so, we replace “result” with the lowercase or
@@ -157,10 +181,10 @@ static class Composer
             }
         }
 
-        // If we are being used to capture a key, send the resulting key
+        // If we are being used to capture a key, send the resulting key.
         if (Captured != null)
         {
-            if (!is_keydown)
+            if (is_keyup)
                 Captured.Invoke(key);
             return true;
         }
@@ -806,6 +830,11 @@ static class Composer
     /// The compose key being used; only valid in state “KeyCombination” for now.
     /// </summary>
     private static Key m_current_compose_key = new Key(VK.NONE);
+
+    /// <summary>
+    /// Whether the last control keypress was AltGr
+    /// </summary>
+    private static bool m_control_key_was_altgr = false;
 
     /// <summary>
     /// Whether the current compose key is AltGr

commit bc9dcdd16273d8c5746bd7248e82a0fc67933fcf

Yet another AltGr fix for a new bug I introduced in 4b6e5d607e4.

src/composer/Composer.cs
@@ -134,25 +134,24 @@ static class Composer
         // Special handling of Left Control on keyboards with AltGr
         if (KeyboardLayout.HasAltGr && key.VirtualKey == VK.LCONTROL)
         {
+            bool is_altgr = false;
             // If this is a key down event with LLKHF_ALTDOWN but no Alt key
             // is down, it is actually AltGr.
-            if (is_keydown && (flags & LLKHF.ALTDOWN) != 0
-                 && ((NativeMethods.GetKeyState(VK.LMENU) |
-                      NativeMethods.GetKeyState(VK.RMENU)) & 0x80) == 0)
-            {
-                m_control_key_was_altgr = true;
-                return true;
-            }
-
-            // If this is a key up event bug Left Control is not down, it is
+            is_altgr |= is_keydown && (flags & LLKHF.ALTDOWN) != 0
+                         && ((NativeMethods.GetKeyState(VK.LMENU) |
+                              NativeMethods.GetKeyState(VK.RMENU)) & 0x80) == 0;
+            // If this is a key up event but Left Control is not down, it is
             // actually AltGr.
-            if (is_keyup && m_control_key_was_altgr)
-            {
-                m_control_key_was_altgr = false;
-                return true;
-            }
+            is_altgr |= is_keyup && m_control_down_was_altgr;
 
-            m_control_key_was_altgr = false;
+            m_control_down_was_altgr = is_keydown && is_altgr;
+
+            // Eat the key if our compose key is altgr
+            if (is_altgr && m_compose_key_is_altgr)
+                return true;
+
+            // Otherwise ignore the key
+            goto exit_ignore_key;
         }
 
         // If Caps Lock is on, and the Caps Lock hack is enabled, we check
@@ -422,6 +421,10 @@ static class Composer
         }
 
         return true;
+exit_ignore_key:
+        Log.Debug("Forwarding {0} “{1}” to system (state: {2})",
+                  is_keydown ? "⭝" : "⭜", key.FriendlyName, m_state);
+        return false;
     }
 
     /// <summary>
@@ -834,7 +837,7 @@ static class Composer
     /// <summary>
     /// Whether the last control keypress was AltGr
     /// </summary>
-    private static bool m_control_key_was_altgr = false;
+    private static bool m_control_down_was_altgr = false;
 
     /// <summary>
     /// Whether the current compose key is AltGr